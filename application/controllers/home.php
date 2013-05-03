96d7944457abe2971ddafa54eaff3b84cdcdebf8
Santiago Toledo <santiagotoledo91@gmail.com>
2013-05-03 16:13:57 -0430

    Thats how you pass a variable to a view, home/index is now clean.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -32,7 +32,13 @@ class Home_Controller extends Base_Controller {
 
 	public function action_index()
 	{
+		// define the $view object as the home.index view loader
 		$view = View::make('home.index');
+		
+		// the thats how you pass variables to views
+		$view->variable = 'This is an variable';
+
+		// assigns the content of the view to the "content" property of the layout
 		$this->layout->content = $view;
 	}
 
