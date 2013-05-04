302f585d956b8fb4010f6c00f3214304c9a6107e
satl91 <santiagotoledo91@gmail.com>
2013-05-04 02:53:25 -0430

    Boostrap added and working with laravel.

--- application/views/layouts/default.blade.php
+++ application/views/layouts/default.blade.php
@@ -1,6 +1,12 @@
 <html>
 <head>
 	<title> {{ $title }} </title> 
+	
+	{{ HTML::style('css/bootstrap.css')}}
+	{{ HTML::style('css/bootstrap-responsive.css')}}
+
+	{{ HTML::script('js/bootstrap.js')}}
+
 </head>
 <body>
 	{{ $header }}
