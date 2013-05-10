66372916b3bca0c4dd1a92ed9f82a46dd8e8d57b
Santiago Toledo <santiagotoledo91@gmail.com>
2013-05-10 10:34:44 -0430

    Admin section header created. JQuery, Collapse, dropdown and transition plugins added. Icons are now gray to match the header style.

--- application/controllers/base.php
+++ application/controllers/base.php
@@ -9,11 +9,15 @@ class Base_Controller extends Controller {
 		parent::__construct();
 		
 		$this->layout->title = 'Construcciones Mencey, C.A';
-			
-		Asset::add('responsive.css','css/bootstrap-responsive.css');
-		Asset::add('bootstrap.css','css/bootstrap.css');
 
+		Asset::add('bootstrap.css','css/bootstrap.css');
+		Asset::add('responsive.css','css/bootstrap-responsive.css');
+		
 		Asset::add('bootstrap.js','js/bootstrap.js');
+		Asset::add('jquery.js','js/jquery.js');
+		Asset::add('bootstrap-dropdown.js','js/bootstrap-dropdown.js');
+		Asset::add('bootstrap-collapse.js','js/bootstrap-collapse.js');
+		Asset::add('bootstrap-transition.js','js/bootstrap-transition.js');
 	}
 
 	public function __call($method, $parameters)
--- application/views/layouts/admin.blade.php
+++ application/views/layouts/admin.blade.php
@@ -1,27 +1,189 @@
-<html>
+<!DOCTYPE html>
+
+<html lang="en">
+
 <head>
+
 	<title> {{ $title }} </title> 
 
 	{{ Asset::styles() }} 
 
 </head>
+
 <body>
 
+	<!-- START HEADER -->
 	<header>
-		
-		this is a header
+
+		<div class="navbar">
+
+			<div class="navbar-inner">
+
+				<div class="container">
+
+
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+
+					<a class="brand">Construcciones Mencey, C.A</a>
+
+					<div class="nav-collapse">
+
+						<ul class="nav">
+
+							<li><a href=" {{ URL::to('admin') }} "><i class="icon-home"></i> Inicio</a></li>
+
+							<li class="divider-vertical"></li>
+
+							<li class="dropdown">
+
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-briefcase"></i> Tesoreria <b class="caret"></b></a>
+
+								<ul class="dropdown-menu">
+
+									<li>
+										<a href="#">Nómina</a>
+									</li>
+
+									<li>
+										<a href="#">Prestaciones sociales</a>
+									</li>
+
+								</ul>
+
+							</li>
+
+							<li class="divider-vertical"></li>
+
+							<li class="dropdown">
+
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i> Personal <b class="caret"></b></a>
+
+								<ul class="dropdown-menu">
+
+									<li>
+										<a href="#"><i class="icon-plus"></i> Agregar</a>
+									</li>
+
+									<li>
+										<a href="#"><i class="icon-pencil"></i> Modificar</a>
+									</li>
+
+									<li>
+										<a href="#"><i class="icon-minus"></i> Eliminar</a>
+									</li>
+
+								</ul>
+
+							</li>
+
+							<li class="divider-vertical"></li>
+
+							<li class="dropdown">
+
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-folder-open"></i> Documentos <b class="caret"></b></a>
+
+								<ul class="dropdown-menu">
+
+									<li>
+										<a href="#"><i class="icon-exclamation-sign"></i> Ver pendientes</a>
+									</li>
+
+									<li class="divider"></li>
+
+									<li>
+										<a href="#"><i class="icon-plus"></i> Añadir</a>
+									</li>
+
+									<li>
+										<a href="#"><i class="icon-minus"></i> Eliminar</a>
+									</li>
+
+								</ul>
+
+							</li>
+
+							<li class="divider-vertical"></li>
+
+							<li class="dropdown">
+
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-print"></i> Imprimir <b class="caret"></b></a>
+
+								<ul class="dropdown-menu">
+
+									<li>
+										<a href="#">Nómina</a>
+									</li>
+
+									<li>
+										<a href="#">Recibos de págo</a>
+									</li>
+
+									<li>
+										<a href="#">Prestaciones sociales</a>
+									</li>
+
+									<li class="divider"></li>
+
+									<li>
+										<a href="#">Solvencia A</a>
+									</li>
+
+									<li>
+										<a href="#">Solvencia B</a>
+									</li>
+
+									<li>
+										<a href="#">Solvencia C</a>
+									</li>
+
+								</ul>
+
+							</li>
+
+						</ul>
+
+						<ul class="nav pull-right">
+
+							<li><a href="admin/logout"><i class="icon-off"></i> Cerrar sesion</a></li>
+
+						</ul>
+
+					</div>	
+
+				</div>
+
+			</div>
+
+		</div>
 
 	</header>
+	<!-- END HEADER -->
 
+	<!-- START CONTENT -->
 	{{ $content }}
+	<!-- END CONTENT -->
 
+	<!-- START FOOTER -->
 	<footer>
-		
+
 		this is a footer
 
 	</footer>
+	<!-- END FOOTER -->
 
 	{{ Asset::scripts() }}
-	
+
+	<script type="text/javascript">  
+		$(document).ready(function () {  
+			$(".collapse").collapse();
+			$(".dropdown-toggle").dropdown();  
+		});
+	</script>
+
 </body>
+
 </html>
