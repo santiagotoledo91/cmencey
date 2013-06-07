b11b4740c2b7ae6f276300d7205e51e97afa32df
Santiago Toledo <santiagotoledo91@gmail.com>
2013-06-07 00:48:06 -0430

    Sticky footer added.

--- application/views/layouts/admin.blade.php
+++ application/views/layouts/admin.blade.php
@@ -11,168 +11,177 @@
 </head>
 
 <body>
+	
+	<!-- START WARP -->
+	<div id="wrap">
 
-	<!-- START HEADER -->
-	<header>
+		<!-- START MAIN -->
+		<div id="main">
 
-		<div class="navbar">
+			<!-- START HEADER -->
+			<div class="navbar">
 
-			<div class="navbar-inner">
+				<div class="navbar-inner">
 
-				<div class="container">
+					<div class="container">
 
 
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</a>
+						<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</a>
 
-					<a class="brand">Construcciones Mencey, C.A</a>
+						<a class="brand">Construcciones Mencey, C.A</a>
 
-					<div class="nav-collapse">
+						<div class="nav-collapse">
 
-						<ul class="nav">
+							<ul class="nav">
 
-							<li><a href=" {{ URL::to('admin') }} "><i class="icon-home"></i> Inicio</a></li>
+								<li><a href=" {{ URL::to('admin') }} "><i class="icon-home"></i> Inicio</a></li>
 
-							<li class="divider-vertical"></li>
+								<li class="divider-vertical"></li>
 
-							<li class="dropdown">
+								<li class="dropdown">
 
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-briefcase"></i> Tesoreria <b class="caret"></b></a>
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-briefcase"></i> Tesoreria <b class="caret"></b></a>
 
-								<ul class="dropdown-menu">
+									<ul class="dropdown-menu">
 
-									<li>
-										<a href="{{ URL::to('admin/paysheets/pre') }}">Generar nómina</a>
-									</li>
+										<li>
+											<a href="{{ URL::to('admin/paysheets/pre') }}">Generar nómina</a>
+										</li>
 
-									<li>
-										<a href="#">Prestaciones sociales</a>
-									</li>
+										<li>
+											<a href="#">Prestaciones sociales</a>
+										</li>
 
-								</ul>
+									</ul>
 
-							</li>
+								</li>
 
-							<li class="divider-vertical"></li>
+								<li class="divider-vertical"></li>
 
-							<li class="dropdown">
+								<li class="dropdown">
 
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i> Personal <b class="caret"></b></a>
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-user"></i> Personal <b class="caret"></b></a>
 
-								<ul class="dropdown-menu">
+									<ul class="dropdown-menu">
 
-									<li>
-										<a href="{{ URL::to('admin/employees/add') }}"><i class="icon-plus"></i> Añadir</a>
-									</li>
+										<li>
+											<a href="{{ URL::to('admin/employees/add') }}"><i class="icon-plus"></i> Añadir</a>
+										</li>
 
-									<li>
-										<a href=" {{ URL::to('admin/employees/manage') }} "><i class="icon-cog"></i> Ver - Editar</a>
-									</li>
+										<li>
+											<a href=" {{ URL::to('admin/employees/manage') }} "><i class="icon-cog"></i> Ver - Editar</a>
+										</li>
 
-								</ul>
+									</ul>
 
-							</li>
+								</li>
 
-							<li class="divider-vertical"></li>
+								<li class="divider-vertical"></li>
 
-							<li class="dropdown">
+								<li class="dropdown">
 
-								<a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-folder-open"></i> Documentos <b class="caret"></b></a>
+									<a href="" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-folder-open"></i> Documentos <b class="caret"></b></a>
 
-								<ul class="dropdown-menu">
+									<ul class="dropdown-menu">
 
-									<li>
-										<a href="{{ URL::to('admin/docs/expired') }}"><i class="icon-exclamation-sign"></i> Vencidos y por vencer</a>
-									</li>
+										<li>
+											<a href="{{ URL::to('admin/docs/expired') }}"><i class="icon-exclamation-sign"></i> Vencidos y por vencer</a>
+										</li>
 
-									<li>
-										<a href="{{ URL::to('admin/docs/pending') }}"><i class="icon-exclamation-sign"></i> Por consignar</a>
-									</li>
+										<li>
+											<a href="{{ URL::to('admin/docs/pending') }}"><i class="icon-exclamation-sign"></i> Por consignar</a>
+										</li>
 
-									<li class="divider"></li>
+										<li class="divider"></li>
 
-									<li>
-										<a href="{{ URL::to('admin/docs/add') }}"><i class="icon-plus"></i> Nuevo tipo de documento</a>
-									</li>
+										<li>
+											<a href="{{ URL::to('admin/docs/add') }}"><i class="icon-plus"></i> Nuevo tipo de documento</a>
+										</li>
 
-									<li>
-										<a href="{{ URL::to('admin/docs/manage') }}"><i class="icon-cog"></i> Editar tipos de documentos</a>
-									</li>
+										<li>
+											<a href="{{ URL::to('admin/docs/manage') }}"><i class="icon-cog"></i> Editar tipos de documentos</a>
+										</li>
 
-								</ul>
+									</ul>
 
-							</li>
+								</li>
 
-							<li class="divider-vertical"></li>
+								<li class="divider-vertical"></li>
 
-							<li class="dropdown">
+								<li class="dropdown">
 
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-print"></i> Imprimir <b class="caret"></b></a>
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-print"></i> Imprimir <b class="caret"></b></a>
 
-								<ul class="dropdown-menu">
+									<ul class="dropdown-menu">
 
-									<li>
-										<a href="{{ URL::to('admin/paysheets/list') }}">Nómina</a>
-									</li>
+										<li>
+											<a href="{{ URL::to('admin/paysheets/list') }}">Nómina</a>
+										</li>
 
-									<li>
-										<a href="#">Recibos de págo</a>
-									</li>
+										<li>
+											<a href="#">Recibos de págo</a>
+										</li>
 
-									<li>
-										<a href="#">Prestaciones sociales</a>
-									</li>
+										<li>
+											<a href="#">Prestaciones sociales</a>
+										</li>
 
-									<li class="divider"></li>
+										<li class="divider"></li>
 
-									<li>
-										<a href="#">Solvencia A</a>
-									</li>
+										<li>
+											<a href="#">Solvencia A</a>
+										</li>
 
-									<li>
-										<a href="#">Solvencia B</a>
-									</li>
+										<li>
+											<a href="#">Solvencia B</a>
+										</li>
 
-									<li>
-										<a href="#">Solvencia C</a>
-									</li>
+										<li>
+											<a href="#">Solvencia C</a>
+										</li>
 
-								</ul>
+									</ul>
 
-							</li>
+								</li>
 
-						</ul>
+							</ul>
 
-						<ul class="nav pull-right">
+							<ul class="nav pull-right">
 
-							<li><a href=" {{ URL::to('admin/logout') }} "><i class="icon-off"></i> Cerrar sesion</a></li>
+								<li><a href=" {{ URL::to('admin/logout') }} "><i class="icon-off"></i> Cerrar sesion</a></li>
 
-						</ul>
+							</ul>
 
-					</div>	
+						</div>	
+
+					</div>
 
 				</div>
 
 			</div>
+			<!-- END HEADER -->
 
-		</div>
+			<!-- START CONTENT -->
+				@yield('content')
+			<!-- END CONTENT -->
 
-	</header>
-	<!-- END HEADER -->
+		</div> 
+		<!-- END MAIN -->
 
-	<!-- START CONTENT -->
-	@yield('content')
-	<!-- END CONTENT -->
+	</div>
+	<!-- END WARP -->
 
 	<!-- START FOOTER -->
-	<footer>
+	<div id="footer">
 
-		this is a footer
+		<p>Construcciones Mencey, C.A - Sistema de administracion de personal</p>
+		<p>Copyright © Santiago Toledo — Todos los derechos reservados.</p>
 
-	</footer>
+	</div>
 	<!-- END FOOTER -->
 
 	{{ Asset::scripts() }}
